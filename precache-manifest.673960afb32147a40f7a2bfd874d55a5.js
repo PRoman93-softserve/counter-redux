3917ec49db8193e4b2a4e653491c9b8441980c3f
PRoman93 <roman_prokopets@mail.ua>
2020-03-17 18:44:56 +0200

    Updates

---
+++ precache-manifest.673960afb32147a40f7a2bfd874d55a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a7f525e7cc96ddc1378df162e2a08a",
+    "url": "/counter-redux/index.html"
+  },
+  {
+    "revision": "403ac25b9711da5ac9c0",
+    "url": "/counter-redux/static/css/main.fc21879b.chunk.css"
+  },
+  {
+    "revision": "c5fb35ba12d3623195d4",
+    "url": "/counter-redux/static/js/2.ba0f3458.chunk.js"
+  },
+  {
+    "revision": "403ac25b9711da5ac9c0",
+    "url": "/counter-redux/static/js/main.be88fa3d.chunk.js"
+  },
+  {
+    "revision": "00bae15afe20807af8b3",
+    "url": "/counter-redux/static/js/runtime-main.02e8cc30.js"
+  }
+]);
